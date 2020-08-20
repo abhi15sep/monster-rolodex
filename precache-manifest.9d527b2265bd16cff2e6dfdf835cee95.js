e93d7babc707f895ef64eb72d8b70977cb1ef3e9
user <abhay_pratap@thbs.com>
2020-08-20 23:00:05 +0100

    Updates

---
+++ precache-manifest.9d527b2265bd16cff2e6dfdf835cee95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c67f3d3c4fb42cc8c3ef5df918fad653",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "6cfa274c58e526bf50c4",
+    "url": "/monster-rolodex/static/css/main.a142fb8a.chunk.css"
+  },
+  {
+    "revision": "f6d2ab47bb337dc705a8",
+    "url": "/monster-rolodex/static/js/2.b67fbdca.chunk.js"
+  },
+  {
+    "revision": "6cfa274c58e526bf50c4",
+    "url": "/monster-rolodex/static/js/main.9c32eea9.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
